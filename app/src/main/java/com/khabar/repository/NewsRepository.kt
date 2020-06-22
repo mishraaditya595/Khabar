dba282aa603d4b1b543c3548e0dbd236bf7a6425
mishraaditya595 <mishraaditya595@gmail.com>
2020-06-22 21:32:22 +0530

    Added functionality to handle network calls for HomeFragment.kt

--- app/src/main/java/com/khabar/repository/NewsRepository.kt
+++ app/src/main/java/com/khabar/repository/NewsRepository.kt
@@ -1,6 +1,9 @@
 package com.khabar.repository
 
+import com.khabar.api.RetrofitInstance
 import com.khabar.db.ArticleDatabase
 
 class NewsRepository(val db: ArticleDatabase) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
--- app/src/main/java/com/khabar/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/khabar/ui/fragments/HomeFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.khabar.MainActivity
 import com.khabar.R
+import com.khabar.adapters.NewsAdapter
 import com.khabar.ui.NewsViewModel
+import com.khabar.util.Resource
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapater: NewsAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -21,6 +28,44 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
+
+        setupRecyclerView()
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response){
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapater.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Toast.makeText(activity, "Error: $message ", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                is Resource.Loading -> { showProgressBar() }
+            }
+        })
+    }
+
+    private fun setupRecyclerView()
+    {
+        newsAdapater = NewsAdapter()
+        home_fragment_rv.apply {
+            adapter = newsAdapater
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun hideProgressBar()
+    {
+        pagination_PB.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar()
+    {
+        pagination_PB.visibility = View.VISIBLE
     }
 
 }
