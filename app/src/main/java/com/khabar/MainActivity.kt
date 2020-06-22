4b7c89d4d77d0e5d202d70f00aaf2037850f0be3
mishraaditya595 <mishraaditya595@gmail.com>
2020-06-22 11:58:10 +0530

    Added Bottom Nav View and fragments

--- app/src/main/java/com/khabar/MainActivity.kt
+++ app/src/main/java/com/khabar/MainActivity.kt
@@ -2,10 +2,58 @@ package com.khabar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.khabar.fragments.HomeFragment
+import com.khabar.fragments.SavedNewsFragment
+import com.khabar.fragments.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(app_toolbar)
+        initialiseFields()
+        loadFragment(HomeFragment())
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.home_item -> {
+                    loadFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.search_item -> {
+                    loadFragment(SearchFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.saved_news_item -> {
+                    loadFragment(SavedNewsFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+        }
+        }
+
+    private fun initialiseFields() {
+        bottomNavigationView=findViewById(R.id.bottom_navigation_view)
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        // load fragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
